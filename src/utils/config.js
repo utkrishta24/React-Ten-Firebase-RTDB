195c40acc0f67b85fad38d3a29f8682718b025f1
Utkrishta Sinha <krishsinha2406@gmail.com>
2023-06-07 00:18:49 +0530

    fixes required

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZNI9u1zweBqbSC9ynivbpcJ36wvu2bdw",
+  authDomain: "react-rtbs.firebaseapp.com",
+  databaseURL: "https://react-rtbs-default-rtdb.firebaseio.com",
+  projectId: "react-rtbs",
+  storageBucket: "react-rtbs.appspot.com",
+  messagingSenderId: "522374362377",
+  appId: "1:522374362377:web:fe2ef5cd8c8de6904dcfd8",
+  measurementId: "G-PQ3F91T2C2"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
